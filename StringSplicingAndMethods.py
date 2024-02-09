055762af63cddaba079ae336712c4d7987b38fae
Anshuman Nigam <119495979+AnshumanNigam@users.noreply.github.com>
2024-02-09 22:42:21 +0530

    Add files via upload

---
+++ StringSplicingAndMethods.py
@@ -0,0 +1,32 @@
+nm="Harry"
+print(nm[-4:-2])
+print(nm[0:4])
+
+str1="Money is Money!!!!"
+str2="anshuman is Good"
+str3="hello world"
+str4="HELLO WORLD"
+str5="Eenie mene mine moe"
+str6="anshuman123AnshumanANSH"
+
+print(str3.upper())
+print(str4.lower())
+print(str1.rstrip("!"))
+print(str1.replace("Money","Honey"))
+print(str2.split(" "))
+print(str2.capitalize())
+print(str4.center)
+print(str5.count("e"))
+print(str5.endswith("moe"))
+print(str5.endswith("ne",0,10))
+
+print(str5.find("in"))
+print(str6.isalnum())
+print(str6.isalpha())
+print(str3.islower())
+print(str5.isprintable())
+print(str6.isspace())
+print(str6.istitle())
+print(str6.swapcase())
+print(str5.title())
+
