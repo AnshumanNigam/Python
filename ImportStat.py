afa648384935771ffc41bc1870ca35151ecc2787
Anshuman Nigam <119495979+AnshumanNigam@users.noreply.github.com>
2024-03-25 23:25:23 +0530

    Add files via upload

---
+++ ImportStat.py
@@ -0,0 +1,22 @@
+# import math
+# print(dir(math))
+
+# result=math.sqrt(9)
+# print(result)
+# print(math.pi)
+
+# import math as m
+# result=m.sqrt(9)
+# print(result)
+
+# from math import sqrt,pi
+# result=sqrt(9)
+# print(result)
+# print(pi)
+
+# from math import*
+# result=sqrt(9)
+# print(result)
+# print(pi)
+
+
